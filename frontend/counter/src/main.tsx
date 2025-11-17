d57abe27a047c7eb37e8200a04f899325b42e36b
Hood-Codivo <godwinfriday34@yahoo.com>
2025-11-17 14:47:52 +0100

    buffer error cleared

--- frontend/counter/src/main.tsx
+++ frontend/counter/src/main.tsx
@@ -1,4 +1,11 @@
 import { createRoot } from "react-dom/client";
+// Ensure Buffer/global are available for Anchor/web3 in browser
+import { Buffer } from "buffer";
+// @ts-ignore: make Buffer/global visible on window/globalThis
+if (!(globalThis as any).Buffer) (globalThis as any).Buffer = Buffer;
+// Some libs expect `global` to exist in browser
+// @ts-ignore
+if (!(globalThis as any).global) (globalThis as any).global = globalThis;
 import "./index.css";
 import { App } from "./App.tsx";
 
